9f712ddcc2df1fa2bd1c946ef0af9c5e7df2b7b5
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-07-06 19:17:57 +0300

    worked

--- src/app/models/room.model.ts
+++ src/app/models/room.model.ts
@@ -1,7 +1,13 @@
 export interface Room {
-    _id: string;
-    room_number: number;
-    capacity: number;
-    comfort_level: string;
-    price: number;
-  }
+  _id: string;
+  room_number: number;
+  capacity: number;
+  comfort_level: string;
+  price: number;
+  bookings: Booking[];
+}
+
+export interface Booking {
+  check_in_date: string;
+  check_out_date: string;
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { catchError, map } from 'rxjs/operators';
 import { Room } from '../models/room.model';
 import { RoomsService } from '../services/rooms.service';
@@ -7,23 +8,38 @@ import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { BookModalComponent } from '../book-modal/book-modal.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { isBefore, isAfter } from 'date-fns';
 
 @Component({
   selector: 'app-rooms',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, FormsModule, MatFormFieldModule, MatDatepickerModule],
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.css'],
+  providers: [provideNativeDateAdapter()],
 })
+
 export class RoomsComponent implements OnInit {
 
   rooms$!: Observable<Room[]>;
+  allRooms: Room[] = [];
   loading = true;
+  startDate: Date | null;
+  endDate: Date | null;
+  filteredRooms: Room[];
 
   constructor(
     private roomsService: RoomsService,
     private dialog: MatDialog
-  ) { }
+  ) {
+    this.startDate = null;
+    this.endDate = null;
+    this.filteredRooms = [];
+  }
 
   ngOnInit() {
     this.fetchRooms();
@@ -31,7 +47,6 @@ export class RoomsComponent implements OnInit {
 
   fetchRooms() {
     this.loading = true;
-
     this.rooms$ = this.roomsService.getRooms().pipe(
       map((response: any) => response.data),
       catchError(error => {
@@ -44,6 +59,8 @@ export class RoomsComponent implements OnInit {
     this.rooms$.subscribe({
       next: (rooms: Room[]) => {
         console.log('Received rooms:', rooms);
+        this.allRooms = rooms; // Зберігаємо всі кімнати
+        this.filteredRooms = rooms; // Початково відображаємо всі кімнати
         this.loading = false;
       },
       error: (error) => {
@@ -57,6 +74,28 @@ export class RoomsComponent implements OnInit {
     });
   }
 
+  filterRooms() {
+    if (!this.startDate || !this.endDate) {
+      return; // Якщо дати не введені, не виконувати фільтрацію
+    }
+
+    // Конвертація введених дат в об'єкти Date
+    const checkInDate = new Date(this.startDate);
+    const checkOutDate = new Date(this.endDate);
+
+    // Фільтрація кімнат на основі введених дат заїзду і виїзду
+    this.filteredRooms = this.allRooms.filter(room => {
+      return !room.bookings.some(booking =>
+        (isBefore(checkInDate, new Date(booking.check_out_date)) && isAfter(checkOutDate, new Date(booking.check_in_date)))
+      );
+    });
+
+    // Оновлення Observable для відображення відфільтрованих кімнат
+    this.rooms$ = of(this.filteredRooms);
+    console.log(this.filteredRooms);
+    console.log(this.rooms$);
+  }
+
   openBookModal(roomId: string): void {
     const dialogRef = this.dialog.open(BookModalComponent, {
       width: '600px',
@@ -67,9 +106,8 @@ export class RoomsComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      console.log(result)
-      if (result) { // Execute fetchRooms only if booking was successful
-
+      console.log(result);
+      if (result) {
         this.fetchRooms();
       }
     });
